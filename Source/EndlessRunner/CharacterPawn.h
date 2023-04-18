1b02160c4e07b216fcb5f8dbfbeeb5e46d9a3892
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-04-18 13:36:32 +0200

    Created CharacterPawn

---
+++ Source/EndlessRunner/CharacterPawn.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CharacterPawn.generated.h"
+
+UCLASS()
+class ENDLESSRUNNER_API ACharacterPawn : public APawn
+{
+	GENERATED_BODY()
+
+private:
+
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(EditAnywhere)
+	float MovementSpeed = 50;
+
+	UPROPERTY(EditAnywhere)
+	float MinValue;
+
+	UPROPERTY(EditAnywhere)
+	float MaxValue;
+
+	void Move(float Value);
+
+	
+public:
+	// Sets default values for this pawn's properties
+	ACharacterPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
